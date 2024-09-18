9ef50c417f1d6957b468cbeeda6b9bf190ff6241
zemzemabdelkarim <81090104+zemzemabdelkarim@users.noreply.github.com>
2024-09-18 10:26:23 +0100

    Feat: Clear button
    Add a clear button to html and add the corresponding event lisenter

--- src.js
+++ src.js
@@ -33,10 +33,17 @@ let resize = document.querySelector("#resize");
 resize.addEventListener("click", () => {
     newSize = prompt("Enter the new size you want : ");
     size = parseInt(newSize);
+    
     while (size < 1 || size > 100 || size === NaN) {
         newSize = prompt("Invalid size, it has to be between 1 and 100 : ");
         size = parseInt(newSize);
     }
 
     createSketch();
+});
+
+let reset = document.querySelector("#reset");
+reset.addEventListener("click",()=>{
+    createSketch();
+    
 });
